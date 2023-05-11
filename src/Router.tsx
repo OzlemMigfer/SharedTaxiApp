d6e71cd54fc830207d18ca8a86f55b2f0b9b4297
Özlem Miğfer <ozlemhayalkurun@gmail.com>
2023-05-12 00:28:37 +0300

    add splash_screen
    add splash_screen and resize logo images

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,22 +1,26 @@
-import { Text, View } from 'react-native';
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SplashScreen from 'react-native-splash-screen';
 
 const Stack = createNativeStackNavigator();
 
 import FirstPage from './pages/auth/FirstPage';
 
-export class Router extends Component {
-  render() {
-    return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="FirstPage" screenOptions={{ headerShown:false }}>
-          <Stack.Screen name="FirstPage" component={FirstPage} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    );
-  }
+const Router = () => {
+
+  useEffect(() => {
+    SplashScreen.hide();
+  },[]);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="FirstPage" screenOptions={{ headerShown:false }}>
+        <Stack.Screen name="FirstPage" component={FirstPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+
 };
 
 export default Router;
