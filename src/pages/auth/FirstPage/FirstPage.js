4b69316ab00a4208b17c3ee3822efe4bd2c1fa66
Özlem Miğfer <ozlemhayalkurun@gmail.com>
2023-05-14 18:32:01 +0300

    add CountryPicker
    add CountryPicker structure for TR

--- src/pages/auth/FirstPage/FirstPage.js
+++ src/pages/auth/FirstPage/FirstPage.js
@@ -1,32 +1,47 @@
-import {Text, View, TextInput, Button} from 'react-native';
 import React, {useState} from 'react';
+import {Text, View, TextInput, Button} from 'react-native';
 import ConfirmOTP from '../ConfirmOTP';
+import CountryPicker from 'react-native-country-picker-modal';
+import styles from './FirstPage.styles';
 
 const FirstPage = () => {
+  const [countryCode, setCountryCode] = useState('TR');
   const [phoneNumber, setPhoneNumber] = useState('');
   const [verificationStarted, setVerificationStarted] = useState(false);
+
 
   const handlePhoneAuth = () => {
     setVerificationStarted(true);
   };
 
   if (verificationStarted) {
+    const fullPhoneNumber = `+90${phoneNumber}`;
     return (
-      <ConfirmOTP phoneNumber={phoneNumber} />
+      <ConfirmOTP phoneNumber={fullPhoneNumber} />
     );
   }
 
   return (
-    <View>
-      <Text>FirstPage</Text>
-      <View>
+    <View style={styles.container}>
+      <View style={styles.country_code}>
+        <CountryPicker
+          countryCode={countryCode}
+          withFilter="false"
+          withFlag
+          withCallingCode={true}
+          onSelect={(country) => setCountryCode(country.cca2)}
+          defaultCode="+90"
+        />
+        <Text style={styles.country_code_text}>{countryCode}</Text>
+
         <TextInput
           value={phoneNumber}
           placeholder="Telefon Numarası"
-          onChangeText={setPhoneNumber}
+          onChangeText={(text) => setPhoneNumber(text)}
+          keyboardType='phone-pad'
         />
-        <Button title="Devam" onPress={handlePhoneAuth} />
       </View>
+      <Button title="Devam" onPress={handlePhoneAuth} />
     </View>
   );
 };
--- src/pages/auth/FirstPage/FirstPage.styles.js
+++ src/pages/auth/FirstPage/FirstPage.styles.js
@@ -1,7 +1,14 @@
-import { StyleSheet,Dimensions } from "react-native/types";
+import { StyleSheet,Dimensions } from "react-native";
 
 export default StyleSheet.create({
     container:{
-
+        flex:1,
+    },
+    country_code:{
+        flexDirection:'row',
+        alignItems:'center'
+    },
+    country_code_text:{
+        marginLeft:0
     }
 });
