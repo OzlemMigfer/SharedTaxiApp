fe0069eda8744d06d1020c2c90a9d67e1ea96fc4
Özlem Miğfer <ozlemhayalkurun@gmail.com>
2023-05-14 23:36:17 +0300

    add firstpage frontEnd design
    add firstpage frontEnd design

--- src/pages/auth/FirstPage/FirstPage.js
+++ src/pages/auth/FirstPage/FirstPage.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
-import {Text, View, TextInput, Button} from 'react-native';
+import {Text, View, TextInput, TouchableOpacity, Image, KeyboardAvoidingView } from 'react-native';
 import ConfirmOTP from '../ConfirmOTP';
-import CountryPicker from 'react-native-country-picker-modal';
+import CountryPicker, {DARK_THEME} from 'react-native-country-picker-modal';
 import styles from './FirstPage.styles';
 
 const FirstPage = () => {
@@ -9,39 +9,63 @@ const FirstPage = () => {
   const [phoneNumber, setPhoneNumber] = useState('');
   const [verificationStarted, setVerificationStarted] = useState(false);
 
-
   const handlePhoneAuth = () => {
     setVerificationStarted(true);
   };
 
   if (verificationStarted) {
     const fullPhoneNumber = `+90${phoneNumber}`;
-    return (
-      <ConfirmOTP phoneNumber={fullPhoneNumber} />
-    );
+    return <ConfirmOTP phoneNumber={fullPhoneNumber} />;
   }
 
   return (
     <View style={styles.container}>
-      <View style={styles.country_code}>
-        <CountryPicker
-          countryCode={countryCode}
-          withFilter="false"
-          withFlag
-          withCallingCode={true}
-          onSelect={(country) => setCountryCode(country.cca2)}
-          defaultCode="+90"
-        />
-        <Text style={styles.country_code_text}>{countryCode}</Text>
+      <Image
+        style={styles.image_logo}
+        source={require('../../../components/assets/logo1.png')}
+      />
+      <View style={styles.country_input}>
+        <View style={styles.country_picker__container}>
+          <CountryPicker
+            theme={DARK_THEME}
+            countryCode={countryCode}
+            withFilter="false"
+            withFlag
+            withCallingCode={true}
+            onSelect={country => setCountryCode(country.cca2)}
+            defaultCode="+90"
+          />
+          <Text style={styles.country_code_text}>{countryCode}</Text>
+        </View>
 
         <TextInput
+          style={styles.phone_input}
           value={phoneNumber}
           placeholder="Telefon Numarası"
-          onChangeText={(text) => setPhoneNumber(text)}
-          keyboardType='phone-pad'
+          placeholderTextColor="gray"
+          onChangeText={text => setPhoneNumber(text)}
+          keyboardType="phone-pad"
         />
       </View>
-      <Button title="Devam" onPress={handlePhoneAuth} />
+      <TouchableOpacity
+        style={styles.button_phone}
+        title="Devam"
+        onPress={handlePhoneAuth}>
+        <Text style={styles.button_continue}>DEVAM</Text>
+      </TouchableOpacity>
+
+      {/* line */}
+      <KeyboardAvoidingView
+        style={styles.container_line}
+        behavior="padding"
+        keyboardVerticalOffset={100}>
+        <View style={styles.line_view}>
+          <View style={styles.line}></View>
+          <View style={styles.text_container_line}>
+            <Text style={styles.text_line}>veya</Text>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
     </View>
   );
 };
--- src/pages/auth/FirstPage/FirstPage.styles.js
+++ src/pages/auth/FirstPage/FirstPage.styles.js
@@ -3,12 +3,83 @@ import { StyleSheet,Dimensions } from "react-native";
 export default StyleSheet.create({
     container:{
         flex:1,
+        backgroundColor:'black'
     },
-    country_code:{
+    image_logo:{
+        alignSelf:'center',
+        marginTop:30
+    },
+    country_input:{
         flexDirection:'row',
-        alignItems:'center'
+        alignItems:'center',
+        alignSelf:'center',
+        marginTop:50
+    },
+    country_picker__container:{
+        flexDirection:'row',
+        alignItems:'center',
+        borderWidth:2.5,
+        borderColor:'#febd59',
+        borderRadius:10,
+        padding:5,
+        paddingRight:10,
+        backgroundColor:'black',
+        marginRight:1
     },
     country_code_text:{
-        marginLeft:0
+        color:'gray'
+    },
+    phone_input:{
+        borderWidth:2.5,
+        borderColor:'#febd59',
+        borderRadius:10,
+        padding:5,
+        width:200,
+        paddingLeft:15,
+        backgroundColor:'black',
+        color:'gray'
+    },
+    button_phone:{
+        backgroundColor:'#3e2ab8',
+        alignItems:'center',
+        padding:10,
+        borderRadius:50,
+        width:280,
+        marginTop:20,
+        alignSelf:'center'
+    },
+    button_continue:{
+        color:'white'
+    },
+    container_line:{
+        
+    },
+    line_view:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center',
+        position:'relative',
+        marginTop:50
+    },
+    line:{
+        position:'absolute',
+        borderBottomWidth:1,
+        borderBottomColor:'gray',
+        width:'80%'
+    },
+    text_container_line:{
+        position: 'absolute',
+        top: '50%',
+        left: '57%',
+        transform: [{ translateX: -50 }, { translateY: -10 }],
+        backgroundColor: 'black',
+        padding: 0,
+        borderRadius: 10,
+    },
+    text_line:{
+        textAlign: 'center',
+        color:'#a39ea3',
+        paddingLeft:10,
+        paddingRight:10
     }
 });
